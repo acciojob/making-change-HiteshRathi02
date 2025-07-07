2b8db25ba0b5aa35abce601f63dbc8e5913bf189
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-07-07 15:29:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const makeChange = (c) => {
-  // your name here
-};
+function makeChange(cents) {
+  const change = {};
+
+  change.q = Math.floor(cents / 25);
+  cents %= 25;
+
+  change.d = Math.floor(cents / 10);
+  cents %= 10;
+
+  change.n = Math.floor(cents / 5);
+  cents %= 5;
+
+  change.p = cents; // whatever is left are pennies
+
+  return change;
+}
+
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
